808697ebfa073cff330228cab684dbe3af3bfddf
Tamás Péter <tamaspeti3451@gmail.com>
2025-03-17 11:45:35 +0100

    Abstracted image sampler

---
+++ Engine/Vulkan/ImageSampler.cpp
@@ -0,0 +1,61 @@
+#include "ImageSampler.h"
+
+Vk::ImageSampler::ImageSampler(const ImageSamplerConfig& config) : 
+	config(config)
+{
+	Init();
+}
+
+Vk::ImageSampler::~ImageSampler()
+{
+	Destroy();
+}
+
+const VkSampler Vk::ImageSampler::Value() const
+{
+	return sampler;
+}
+
+void Vk::ImageSampler::Init()
+{
+	auto device = VulkanContext::GetContext()->GetDevice();
+	auto physicalDevice = VulkanContext::GetContext()->GetPhysicalDevice();
+
+	VkSamplerCreateInfo samplerInfo{};
+	samplerInfo.sType = VK_STRUCTURE_TYPE_SAMPLER_CREATE_INFO;
+	samplerInfo.magFilter = config.magFilter;
+	samplerInfo.minFilter = config.minFilter;
+	samplerInfo.addressModeU = config.addressMode;
+	samplerInfo.addressModeV = config.addressMode;
+	samplerInfo.addressModeW = config.addressMode;
+
+	if (config.anisotropyEnable)
+	{
+		VkPhysicalDeviceProperties properties{};
+		vkGetPhysicalDeviceProperties(physicalDevice->Value(), &properties);
+		samplerInfo.anisotropyEnable = VK_TRUE;
+		samplerInfo.maxAnisotropy = properties.limits.maxSamplerAnisotropy;
+	}
+
+	samplerInfo.borderColor = config.borderColor;
+	samplerInfo.unnormalizedCoordinates = VK_FALSE;
+	samplerInfo.compareEnable = VK_FALSE;
+	samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
+	samplerInfo.mipmapMode = config.mipMapFilter;
+	samplerInfo.mipLodBias = 0.0f;
+	samplerInfo.minLod = 0.0f;
+	samplerInfo.maxLod = 0.0f;
+
+	VK_CHECK_MESSAGE(vkCreateSampler(device->Value(), &samplerInfo, nullptr, &sampler), "Failed to create texture sampler!");
+}
+
+void Vk::ImageSampler::Destroy()
+{
+	auto device = VulkanContext::GetContext()->GetDevice();
+
+	if (sampler != VK_NULL_HANDLE)
+	{
+		vkDestroySampler(device->Value(), sampler, nullptr);
+		sampler = VK_NULL_HANDLE;
+	}
+}
---
+++ Engine/Vulkan/ImageSampler.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "VulkanContext.h"
+
+namespace Vk
+{
+	struct ENGINE_API ImageSamplerConfig
+	{
+		VkFilter magFilter;
+		VkFilter minFilter;
+		VkSamplerMipmapMode mipMapFilter;
+		VkSamplerAddressMode addressMode;
+		VkBool32 anisotropyEnable;
+		VkBorderColor borderColor;
+	};
+
+	class ENGINE_API ImageSampler
+	{
+	public:
+		ImageSampler(const ImageSamplerConfig& specification);
+		~ImageSampler();
+		const VkSampler Value() const;
+	private:
+		void Init();
+		void Destroy();
+		ImageSamplerConfig config;
+		VkSampler sampler = VK_NULL_HANDLE;
+	};
+}
